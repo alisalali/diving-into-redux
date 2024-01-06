5999ea34b3a77d28581519312ba3e00a590bbf22
alisalali <alis.alali@hotmail.com>
2024-01-06 06:21:20 +0300

    init redux store with useSelector,reducer,dispatch,action

---
+++ src/components/Counter.js
@@ -0,0 +1,44 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useCallback } from "react";
+
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  const counter = useSelector((state) => state.counter); // to read store state object
+  const show = useSelector((state) => state.showCounter); // to read store state object
+
+  const dispatch = useDispatch(); // initialize dispatch function
+
+  const incrementHandler = useCallback(
+    () => dispatch({ type: "increment" }),
+    []
+  );
+
+  const decrementHandler = useCallback(
+    () => dispatch({ type: "decrement" }),
+    []
+  );
+  const increaseHandler = useCallback(
+    () => dispatch({ type: "increase", amount: 10 }),
+    []
+  );
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5 </button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
